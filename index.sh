3b39c0c1187203e6a31720952df03e5af7827e2d
afeiship <1290657123@qq.com>
2020-11-14 08:42:44 +0800

    feat: add shell like operations

--- index.sh
+++ index.sh
@@ -2,6 +2,13 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-alias t="tmux"
+alias t="tmux";
+alias t-ls="tmux ls";
+alias t-mv="tmux rename-session -t";
+alias t-touch="tmux new -s";
+alias t-go="tmux switch -t";
+alias t-open="tmux attach -t";
+alias t-close="tmux dettach -t";
+alias t-kill="tmux kill-session -t";
 
 unset ROOT_PATH;
